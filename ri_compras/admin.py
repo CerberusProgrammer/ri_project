9b9956f8649cd67ae359707a854efc771c94a604
Omar Flores <cerberusprogrammer@gmail.com>
2023-09-13 15:06:17 -0700

    Implemented the new models for admin view.

--- ri_compras/admin.py
+++ ri_compras/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Usuarios
 from .models import Departamento
+from .models import Producto
+from .models import Componente
 
 admin.site.register(Usuarios)
 admin.site.register(Departamento)
+admin.site.register(Producto)
+admin.site.register(Componente)
