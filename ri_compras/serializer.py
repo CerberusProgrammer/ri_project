bc0f0c54c5ebdd3ee95c1189562809846347fc0f
Omar Flores <cerberusprogrammer@gmail.com>
2023-09-29 21:22:30 -0700

    "Divisa" parameter added to Servicio.

--- ri_compras/serializer.py
+++ ri_compras/serializer.py
@@ -63,7 +63,7 @@ class ProductoSerializer(serializers.ModelSerializer):
 class ServicioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servicio
-        fields = ['id', 'nombre', 'descripcion', 'costo']
+        fields = ['id', 'nombre', 'descripcion', 'costo', 'divisa']
 
 class ProveedorSerializer(serializers.ModelSerializer):
     class Meta:
