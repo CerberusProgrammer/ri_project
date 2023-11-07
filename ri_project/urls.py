c1d55bfe76106a4fb907a564eeb6fad08f0d18f8
Omar Flores <cerberusprogrammer@gmail.com>
2023-11-06 16:45:59 -0800

    New app.

--- ri_project/urls.py
+++ ri_project/urls.py
@@ -25,6 +25,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ri_compras.urls')),
+    path('api/', include('ri_production.urls')),
     path('api/doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
