54259a43cb8ac3c07858da5f683a81c7223e555b
Omar Flores <cerberusprogrammer@gmail.com>
2023-12-29 16:59:22 -0800

    Merge branch 'main' of https://github.com/CerberusProgrammesr/ri_project

--- ri_project/settings.py
+++ ri_project/settings.py
@@ -71,9 +71,9 @@ WSGI_APPLICATION = 'ri_project.wsgi.application'
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'app_ri_database.sqlite3',
-   }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'app_ri_database.sqlite3',
+    }
 }
 
 # DATABASES = {
