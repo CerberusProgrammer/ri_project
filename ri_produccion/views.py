3ed2e7c42043bd5ec9f609924208a55d20ee220b
Omar Flores <cerberusprogrammer@gmail.com>
2024-01-03 10:40:39 -0800

    a

--- ri_produccion/views.py
+++ ri_produccion/views.py
@@ -1624,13 +1624,13 @@ class PiezaViewSet(viewsets.ModelViewSet):
     
     @action(detail=False, methods=['get'])
     def obtener_piezas_sin_procesos(self, request):
-        # Subquery to check if a Placa has any associated Proceso
-        has_proceso = Proceso.objects.filter(placa=OuterRef('pk')).values('pk')
+        # Subquery to check if a Placa has any associated Proceso with the same id
+        has_matching_proceso = Proceso.objects.filter(placa=OuterRef('pk'), placa__id=OuterRef('placas__id')).values('pk')
 
-        # Get all Pieza objects where none of their Placas have a Proceso and requiere_nesteo=True
+        # Get all Pieza objects where none of their Placas have a matching Proceso and requiere_nesteo=True
         # OR where requiere_nesteo=False and placas__isnull=True
         piezas_sin_procesos = Pieza.objects.filter(
-            Q(placas__isnull=False, requiere_nesteo=True, placas__pk__in=Placa.objects.filter(~Exists(has_proceso))) |
+            Q(placas__isnull=False, requiere_nesteo=True, placas__pk__in=Placa.objects.filter(~Exists(has_matching_proceso))) |
             Q(placas__isnull=True, requiere_nesteo=False),
             material__isnull=False,
             estatus='aprobado',
