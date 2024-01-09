95153e6be01521a37396d0ada777f5449cf0512d
Omar Flores <cerberusprogrammer@gmail.com>
2024-01-09 10:43:33 -0800

    Lio

---
+++ devil.py
@@ -0,0 +1,21 @@
+import django
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')  # Reemplaza 'myproject.settings' con la ruta a tu archivo de configuración de Django
+django.setup()
+
+from django.db import connection
+from ri_compras.models import ProductoRequisicion, Requisicion  # Reemplaza 'ri_compras.models' con la ruta a tus modelos
+
+def reset_sequences():
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT setval(pg_get_serial_sequence('"ri_compras_productorequisicion"', 'id'), 
+            COALESCE((SELECT MAX(id)+1 FROM ri_compras_productorequisicion), 1), false);
+        """)
+        cursor.execute("""
+            SELECT setval(pg_get_serial_sequence('"ri_compras_requisicion"', 'id'), 
+            COALESCE((SELECT MAX(id)+1 FROM ri_compras_requisicion), 1), false);
+        """)
+
+reset_sequences()
