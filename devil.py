7a2e0d1051d0ffea703fcb597065a83e1bf40013
Omar Flores <cerberusprogrammer@gmail.com>
2024-01-10 09:17:09 -0800

    Script modificado para arreglar la tabla de ordenes.

--- devil.py
+++ devil.py
@@ -9,13 +9,19 @@ from ri_compras.models import ProductoRequisicion, Requisicion  # Reemplaza 'ri_
 
 def reset_sequences():
     with connection.cursor() as cursor:
+        # cursor.execute("""
+        #     SELECT setval(pg_get_serial_sequence('"ri_compras_productorequisicion"', 'id'), 
+        #     COALESCE((SELECT MAX(id)+1 FROM ri_compras_productorequisicion), 1), false);
+        # """)
+        
+        # cursor.execute("""
+        #     SELECT setval(pg_get_serial_sequence('"ri_compras_requisicion"', 'id'), 
+        #     COALESCE((SELECT MAX(id)+1 FROM ri_compras_requisicion), 1), false);
+        # """)
+        
         cursor.execute("""
-            SELECT setval(pg_get_serial_sequence('"ri_compras_productorequisicion"', 'id'), 
-            COALESCE((SELECT MAX(id)+1 FROM ri_compras_productorequisicion), 1), false);
-        """)
-        cursor.execute("""
-            SELECT setval(pg_get_serial_sequence('"ri_compras_requisicion"', 'id'), 
-            COALESCE((SELECT MAX(id)+1 FROM ri_compras_requisicion), 1), false);
+            SELECT setval(pg_get_serial_sequence('"ri_compras_ordenes"', 'id'), 
+            COALESCE((SELECT MAX(id)+1 FROM ri_compras_ordenes), 1), false);
         """)
 
 reset_sequences()
