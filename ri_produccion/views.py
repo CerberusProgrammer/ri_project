a352fb38446c4b5f97094ae0df430089411e08e4
Omar Flores <cerberusprogrammer@gmail.com>
2024-01-10 15:21:26 -0800

    Nuevo filtro integrado a las funciones

--- ri_produccion/views.py
+++ ri_produccion/views.py
@@ -461,27 +461,19 @@ class ProcesoViewSet(viewsets.ModelViewSet):
             Q(estatus__in=['pendiente', 'operando']),
             Q(finProceso__lt=current_time)
         )
-        response = []
-        for proceso in procesos:
-            piezas = proceso.pieza_set.all()
-            for pieza in piezas:
-                response.append({
-                    'id': proceso.id,
-                    'nombre': proceso.nombre,
-                    'estatus': proceso.estatus,
-                    'maquina': proceso.maquina,
-                    'inicioProceso': proceso.inicioProceso,
-                    'finProceso': proceso.finProceso,
-                    'terminadoProceso': proceso.terminadoProceso,
-                    'comentarios': proceso.comentarios,
-                    'prioridad': proceso.prioridad,
-                    'piezasRealizadas': proceso.piezasRealizadas,
-                    'realizadoPor': proceso.realizadoPor.id if proceso.realizadoPor else None,
-                    'pieza_id': pieza.id,
-                    'consecutivo': pieza.consecutivo
-                })
+        
+        serializer = ProcesoSerializer(procesos, many=True)
+        data = serializer.data
 
-        return Response(response)
+        for proceso_data, proceso in zip(data, procesos):
+            pieza = proceso.pieza_set.first()
+            if pieza:
+                proceso_data['consecutivo'] = pieza.consecutivo
+                pieza_placa = PiezaPlaca.objects.filter(pieza=pieza, placa=proceso.placa).first()
+                if pieza_placa:
+                    proceso_data['piezas_realizadas'] = pieza_placa.piezas_realizadas
+
+        return Response(data)
 
     @action(detail=False, methods=['get'], url_path='mis_procesos_prioritarios/(?P<id>\d+)')
     def mis_procesos_prioritarios(self, request, id=None):
@@ -492,27 +484,18 @@ class ProcesoViewSet(viewsets.ModelViewSet):
             Q(pieza__in=piezas_prioritarias)
         ).distinct().order_by('inicioProceso')
 
-        response = []
-        for proceso in procesos:
-            piezas = proceso.pieza_set.all()
-            for pieza in piezas:
-                response.append({
-                    'id': proceso.id,
-                    'nombre': proceso.nombre,
-                    'estatus': proceso.estatus,
-                    'maquina': proceso.maquina,
-                    'inicioProceso': proceso.inicioProceso,
-                    'finProceso': proceso.finProceso,
-                    'terminadoProceso': proceso.terminadoProceso,
-                    'comentarios': proceso.comentarios,
-                    'prioridad': proceso.prioridad,
-                    'piezasRealizadas': proceso.piezasRealizadas,
-                    'realizadoPor': proceso.realizadoPor.id if proceso.realizadoPor else None,
-                    'pieza_id': pieza.id,
-                    'consecutivo': pieza.consecutivo
-                })
+        serializer = ProcesoSerializer(procesos, many=True)
+        data = serializer.data
 
-        return Response(response)
+        for proceso_data, proceso in zip(data, procesos):
+            pieza = proceso.pieza_set.first()
+            if pieza:
+                proceso_data['consecutivo'] = pieza.consecutivo
+                pieza_placa = PiezaPlaca.objects.filter(pieza=pieza, placa=proceso.placa).first()
+                if pieza_placa:
+                    proceso_data['piezas_realizadas'] = pieza_placa.piezas_realizadas
+
+        return Response(data)
 
     @action(detail=False, methods=['get'], url_path='procesos_maquinado_retrasados_piezas')
     def procesos_maquinado_retrasados_piezas(self, request):
