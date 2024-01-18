c91a62a1fb32037b9f30c08197e28cf61a07f852
Omar Flores <cerberusprogrammer@gmail.com>
2024-01-18 10:12:39 -0800

    Error de parametro

--- ri_produccion/views.py
+++ ri_produccion/views.py
@@ -1531,7 +1531,7 @@ class PiezaViewSet(viewsets.ModelViewSet):
         ).distinct().count()
 
         return Response({
-            "progreso": piezas,
+            "piezas": piezas,
         })
     
     @action(detail=False, methods=['get'], url_path='obtener_piezas_terminadas')
