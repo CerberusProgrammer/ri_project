31d253e9a18eeb77aa0e7ec0255b55356799bfaf
Moises <a17490364@itmexicali.edu.mx>
2024-01-25 09:18:22 -0800

    function obtener_piezas_pendiente_de_nesteo_conteneo

--- ri_produccion/views.py
+++ ri_produccion/views.py
@@ -1935,6 +1935,17 @@ class PiezaViewSet(viewsets.ModelViewSet):
         serializer = PiezaSerializer(piezas_aprobadas, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+     @action(detail=False, methods=['get'], permission_classes=[AllowAny])
+     def obtener_piezas_pendientes_de_nesteo_conteo(self, request):
+        piezas = Pieza.objects.filter(
+            estatus='aprobado',
+            placas__isnull=False
+        ).count()
+
+        return  return Response({"piezas": piezas}, status=status.HTTP_200_OK)
+
+    
+
     @action(detail=False, methods=['get'], permission_classes=[AllowAny])
     def progreso_de_piezas_pendientes_de_procesos(self, request):
         total_piezas = Pieza.objects.filter(
