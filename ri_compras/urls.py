d2b1dec14d406f815cb1e9f8b00cece2bda372e3
Omar Flores <cerberusprogrammer@gmail.com>
2024-02-20 16:51:57 -0800

    Vinculacion de URL de la API a los productos de almacen

--- ri_compras/urls.py
+++ ri_compras/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.urls import include
 from rest_framework.routers import DefaultRouter
-from .views import DepartamentoViewSet
+from .views import DepartamentoViewSet, ProductoAlmacenViewSet
 from .views import UsuariosViewSet
 from .views import ProductoViewSet
 from .views import RequisicionViewSet
@@ -19,6 +19,7 @@ router = DefaultRouter()
 router.register(r'departamentos', DepartamentoViewSet)
 router.register(r'usuarios', UsuariosViewSet)
 router.register(r'productos', ProductoViewSet)
+router.register(r'productos_almacen', ProductoAlmacenViewSet)
 router.register(r'servicios', ServicioViewSet)
 router.register(r'requisiciones', RequisicionViewSet)
 router.register(r'proveedores', ProveedorViewSet)
